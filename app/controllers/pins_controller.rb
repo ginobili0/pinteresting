63856561118083cd1203f5301043910b80f4d6f3
ginobili0 <yaswateja@gmail.com>
2015-06-30 16:33:15 -0400

    Added pagination

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -5,7 +5,7 @@ class PinsController < ApplicationController
   # instead of except we can also use only as mentioned in the upper line and mention all the pages for which we want users to be authenticated
 
   def index
-    @pins = Pin.all.order 'created_at DESC'
+    @pins = Pin.all.order('created_at DESC').paginate(:page => params[:page], :per_page => 5)
   end
 
   def show
